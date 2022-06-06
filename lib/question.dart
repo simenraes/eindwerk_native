2b29c3fab73734d9b3b97fd295b213f3340ddf6c
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-07 01:39:45 +0200

    starting to set up timer

--- lib/question.dart
+++ lib/question.dart
@@ -1,39 +1,50 @@
 import 'package:flutter/material.dart';
 import 'answer.dart';
 import 'bibliotheek.dart' as lib;
-class Question extends StatelessWidget {
 
+class Question extends StatelessWidget {
   // final String questionText;
   int vraagNummer = 0;
-
-
 
   Question(this.vraagNummer);
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> header = <Widget> [
-
+    List<Widget> header = <Widget>[
       Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            ConstrainedBox(
-              constraints: BoxConstraints(minHeight: 50),
-              child: Container(
-                  child: Text(lib.questions[vraagNummer]['questionText'], style: lib.kopTekst, textAlign: TextAlign.center,)
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          ConstrainedBox(
+            constraints: BoxConstraints(minHeight: 50),
+            child: Container(
+                child: Text(
+              lib.questions[vraagNummer]['questionText'],
+              style: lib.kopTekst,
+              textAlign: TextAlign.center,
+            )),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Center(
+                child: Text(
+                  "30",
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Times New Roman',
+                  ),
+                ),
               ),
             ),
+          )
+        ],
+      ),
+    ];
 
-          ],
-        ),
-
-
-      ];
-
-    return ListView(
-    children: [...header, ...maakAntwoordRijen()]
-    );
+    return ListView(children: [...header, ...maakAntwoordRijen()]);
   }
+
   List<Container> maakAntwoordRijen() {
     List<Container> rijen = <Container>[];
     int answerQuantity = (lib.questions[vraagNummer]['answers'].length);
@@ -42,28 +53,16 @@ class Question extends StatelessWidget {
       List<Answer> knoppen = <Answer>[];
       knoppen.add(Answer(lib.questions[vraagNummer]['answers'][rij]['text']));
 
-
       rijen.add(Container(
         constraints: BoxConstraints(minWidth: 20, maxWidth: 50),
-
-
         width: 2,
         padding: EdgeInsets.all(10),
-
-
         child: Center(
           child: Row(
             children: knoppen,
           ),
         ),
-
-
-
-
-      )
-
-
-      );
+      ));
     }
 
     return rijen;
