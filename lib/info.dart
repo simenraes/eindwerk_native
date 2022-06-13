5d72261d4d5162d97544f34c248c5c379108a918
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-13 21:01:17 +0200

    add assets folder

--- lib/info.dart
+++ lib/info.dart
@@ -2,53 +2,9 @@ import 'package:flutter/material.dart';
 import 'bibliotheek.dart' as lib;
 
 
-class Info extends StatefulWidget {
-  const Info({Key? key}) : super(key: key);
-
-  @override
-  _InfoState createState() => _InfoState();
-}
-
-class _InfoState extends State<Info> with TickerProviderStateMixin {
-  bool _isPlay = false;
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    _controller =
-        AnimationController(duration: const Duration(seconds: 1), vsync: this);
-    super.initState();
-  }
-
-  // Dispose the controller
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class Info extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              if (_isPlay == false) {
-                _controller.forward();
-                _isPlay = true;
-              } else {
-                _controller.reverse();
-                _isPlay = false;
-              }
-            },
-            child: AnimatedIcon(
-              icon: AnimatedIcons.play_pause,
-              progress: _controller,
-              size: 200,
-              color: Colors.blue,
-            ),
-          ),
-        ));
+    return Container(color: Colors.yellow);
   }
 }
