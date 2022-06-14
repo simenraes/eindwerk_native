8c2ce65c1e5698a220b9959df87ef92fdc546096
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-14 14:12:07 +0200

    use tekstveldController maildialog

--- lib/mailDialog.dart
+++ lib/mailDialog.dart
@@ -13,7 +13,10 @@ class MailDialoog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Voer jouw e-mailadres in'),
-      content: TextField(decoration: InputDecoration(hintText: 'naam@domein.be'),),
+      content: TextField(
+        controller: tekstveldController,
+
+        decoration: InputDecoration(hintText: 'naam@domein.be'),),
       actions: [
         TextButton(
           child: Text('Annuleer'),
