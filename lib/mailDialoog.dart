1b0ce277c72b466de2358cdfe86c160d6354554f
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-14 17:02:35 +0200

    input wordt geprintin console

--- lib/mailDialoog.dart
+++ lib/mailDialoog.dart
@@ -3,9 +3,29 @@ import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 
 class MailDialoog extends StatelessWidget {
+  final int score;
+  final TextEditingController tekstveldController = TextEditingController();
+
+  MailDialoog(this.score);
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow,);
+    return AlertDialog(
+      title: Text('Voer jouw e-mailadres in'),
+      content: TextField(
+        controller: tekstveldController,
+        decoration: InputDecoration(hintText: 'naam@domein.be'),
+      ),
+      actions: [
 
+        TextButton(
+          child: Text('Verstuur'),
+          onPressed: () {
+            print(tekstveldController.text);
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
   }
 }
