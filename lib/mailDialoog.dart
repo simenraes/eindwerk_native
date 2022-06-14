bc8ee46b68e9a6d3bc07fa55f792aba050c16d55
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-14 18:57:00 +0200

    mail dialog works on mobile

--- lib/mailDialoog.dart
+++ lib/mailDialoog.dart
@@ -3,10 +3,37 @@ import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 
 class MailDialoog extends StatelessWidget {
-  final int score;
+  final int totalscore;
   final TextEditingController tekstveldController = TextEditingController();
 
-  MailDialoog(this.score);
+  MailDialoog(this.totalscore);
+
+  stuurMail() async {
+    const String gebruikersnaam = 'simieraes@hotmail.com';
+    const String paswoord = 'zwartalalasus1';
+
+    final smtpServer = SmtpServer("smtp.office365.com",
+        port: 587,
+        allowInsecure: true,
+        ignoreBadCertificate: true,
+        username: gebruikersnaam,
+        password: paswoord
+    );
+
+    final message = Message();
+    message.from = Address(gebruikersnaam, 'Flutter Quiz');
+    message.recipients.add(tekstveldController.text);
+    message.subject = 'Uitslag van de quiz';
+    message.html = "<h1>Uitslag</h1>\n" + "<p>Je score is $totalscore</p>";
+
+    try {
+      await send(message, smtpServer);
+      print('Succesvol verzonden');
+    } catch(error) {
+      print(error);
+      print('niegegegegegloekt');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +49,7 @@ class MailDialoog extends StatelessWidget {
           child: Text('Verstuur'),
           onPressed: () {
             print(tekstveldController.text);
+            stuurMail();
             Navigator.of(context).pop();
           },
         )
@@ -29,3 +57,5 @@ class MailDialoog extends StatelessWidget {
     );
   }
 }
+
+
