808e2a7157e2e60b3c2ddca7b64554f1efa8db86
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-14 13:45:15 +0200

    made dart file mailDialog

---
+++ lib/mailDialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+
+class MailDialoog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
