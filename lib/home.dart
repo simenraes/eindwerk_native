cf27143fe91b9f142355cf22bcf57a4c8131e23a
simen raes <75736482+simenraes@users.noreply.github.com>
2022-06-14 22:08:46 +0200

    merge master

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
-import 'bibliotheek.dart' as lib;
+import 'package:video_player/video_player.dart';
+
 
 
 class Home extends StatelessWidget {
-  @override
+  Widget build(BuildContext buildContext) => MaterialApp(
+    home: Scaffold(
+      body: AppTree(),
+    ),
+  );
+}
+
+class AppTree extends StatelessWidget {
+  VideoPlayerController videoController = VideoPlayerController.network("https://boek.flutter.nl/geograaf.mp4");
+
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow);
+    videoController.initialize();
+    videoController.play();
+    return VideoPlayer(videoController);
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,37 +1,65 @@
 import 'package:flutter/material.dart';
 import '../bibliotheek.dart' as lib;
+import 'package:audioplayers/audioplayers.dart';
+import 'mailDialoog.dart';
 
 
-import 'package:flutter/material.dart';
 
-class Result extends StatelessWidget {
-   int totalScore;
-   // String resultText;
-  // final Function restartQuiz;
+
+
+class Result extends StatefulWidget {
+  int totalScore;
+
   Result(this.totalScore);
+
+
+
+
+
+  @override
+  _ResultState createState() => _ResultState();
+}
+
+class _ResultState extends State<Result> {
+  final AudioCache audioSpeler = AudioCache();
+
+
+  @override
+  void initState(){
+    audioSpeler.play('audio/applause-2.mp3', volume: 1);
+    super.initState();
+  }
 
   Map<String, Object> get result {
     String resultText;
     Color textColor;
     Color background;
-    if (totalScore <= 8) {
+    String imageUrl;
+    if (widget.totalScore <= 8) {
       resultText = 'You are gentle, pure, quiet and innocent';
       background = Colors.white;
       textColor = Colors.pink;
-    } else if (totalScore <= 12) {
+      imageUrl = "assets/images/baby3.png";
+    } else if (widget.totalScore <= 12) {
       resultText = 'You are pretty likeable, joyful and alive!';
       background = Colors.orange;
       textColor = Colors.green;
-    } else if (totalScore <= 16) {
+      imageUrl = "assets/images/baby4.png";
+
+    } else if (widget.totalScore <= 16) {
       resultText = 'You can be sometimes nervous, nasty or even strange';
       background = Colors.red;
       textColor = Colors.white;
+      imageUrl = "assets/images/nervous.png";
+
     } else {
       resultText = 'You are wounded, violent, often angry, even sometimes dangerous or destructive';
       background = Colors.black;
       textColor = Colors.red;
+      imageUrl = "assets/images/danger.png";
+
     }
-    return {'text': resultText, 'textColor': textColor, 'background': background};
+    return {'text': resultText, 'textColor': textColor, 'background': background, 'imageurl': imageUrl};
   }
   @override
   Widget build(BuildContext context) {
@@ -43,13 +71,16 @@ class Result extends StatelessWidget {
         width: double.infinity,
         padding: EdgeInsets.all(50),
         child: Column(
+
+
           mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Image.asset(result['imageurl'].toString(), width: 150, height: 110,),
             Text(
               result['text'].toString(),
               style: const TextStyle(
-                  fontSize: 36,
+                  fontSize: 15,
                   color: Colors.purple,
                   fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
@@ -64,6 +95,28 @@ class Result extends StatelessWidget {
                 ),
                 child: const Text('Opnieuw', style: lib.basisTekst, textScaleFactor: 1.2),
                 onPressed: () => lib.startOpnieuw.add(true),
+              ),
+            ),
+            Container(
+                height: 60,
+                child: Center(
+                  child: Text("Deel je uitslag", style: lib.kopTekst,),
+                )
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white
+                  ),
+                  child: Text('Versturen', style: lib.basisTekst, textScaleFactor: 1.2),
+                  onPressed: () => {
+                    showDialog(
+                        context: context,
+                        builder: (context) => MailDialoog(widget.totalScore)
+                    )
+                  }
               ),
             )
           ],
